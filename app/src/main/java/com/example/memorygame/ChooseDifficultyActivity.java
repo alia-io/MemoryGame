a1dc95926d365a991c529487b89d09beac0efca5
Alifa Stith <alifastith@gmail.com>
2021-04-21 18:29:18 -0400

    first activity layout

---
+++ app/src/main/java/com/example/memorygame/ChooseDifficultyActivity.java
@@ -0,0 +1,27 @@
+package com.example.memorygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ChooseDifficultyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_difficulty);
+    }
+
+    public void easy(View view) {
+
+    }
+
+    public void medium(View view) {
+
+    }
+
+    public void difficult(View view) {
+
+    }
+}
